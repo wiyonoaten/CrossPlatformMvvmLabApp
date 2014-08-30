5a37c26406fb7e0b9c1cd9bdd566f03d33a1b62d
Wiyono Aten <wiyono.aten@gmail.com>
2014-08-30 16:28:45 +0100

    Added the Xamarin.Forms-based app implementation for WP80 Silverlight.

---
+++ PresentationLib.XF.WP8SL/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Phone.Controls;
+using Xamarin.Forms;
+
+namespace PresentationLib.Views
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+            this.ContentPanel.Children.Add(App.GetMainPage().ConvertPageToUIElement(this));
+        }
+    }
+}
---
+++ XPlatMvvmLabApp.XF.WP8SL/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XPlatMvvmLabApp.Resources;
+
+namespace XPlatMvvmLabApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
